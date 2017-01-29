752d681365b7583b6e0859ad84d24e3ce90a288a
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-01-29 21:56:18 +0200

    Add files via upload

---
+++ Arrays/LargestCommonEnd/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+class LargestCommonEnd
+{
+    static void Main(string[] args)
+    {
+        string[] wordsFirst = Console.ReadLine().Split(' ').ToArray();
+        string[] wordsSecond = Console.ReadLine().Split(' ').ToArray();
+        int countLeftRight = 0;
+        int countRightLeft = 0;
+        for (int i = 0; i < wordsFirst.Length && i < wordsSecond.Length; i++)
+        {
+            if (wordsFirst[i] == wordsSecond[i])
+            {
+                countLeftRight++;
+            }
+        }
+        for (int j = 0; j < wordsFirst.Length && j < wordsSecond.Length; j++)
+        {
+            if (wordsFirst[wordsFirst.Length - 1 - j] == wordsSecond[wordsSecond.Length - 1 - j])
+            {
+                countRightLeft++;
+            }
+        }
+        Console.WriteLine(countRightLeft > countLeftRight ? countRightLeft : countLeftRight);
+
+    }
+}
