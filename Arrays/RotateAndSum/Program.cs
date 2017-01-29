4386317c955a16174fcc622efa4a140e5e784b0c
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-01-29 22:00:40 +0200

    Add files via upload

---
+++ Arrays/RotateAndSum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotateAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine().Split(' ');
+            int[] arrOfNums = new int[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                arrOfNums[i] = int.Parse(numbers[i]);
+            }
+            int rotation = int.Parse(Console.ReadLine());
+            int[] sumAray = new int[numbers.Length];
+            for (int i = 0; i < rotation; i++)
+            {
+                int lastNumber = arrOfNums[numbers.Length - 1];
+
+                for (int j = arrOfNums.Length - 1; j > 0; j--)
+                {
+                    arrOfNums[j] = arrOfNums[j - 1];
+                }
+                arrOfNums[0] = lastNumber;
+
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    sumAray[j] += arrOfNums[j];
+                }
+            }
+            Console.WriteLine(string.Join(" ", sumAray));
+
+        }
+
+    }
+}
+}
