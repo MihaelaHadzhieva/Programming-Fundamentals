078ce6558a3dda453a20686a26947768d981e250
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-03-08 23:59:16 +0200

    Add files via upload

---
+++ Dictionaries-LambdaAndLinq/LargestThreeNums/LargestThreeNums.cs
@@ -0,0 +1,21 @@
+﻿
+namespace LargestThreeNums
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class LargestThreeNums
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .OrderByDescending(x => x)
+                .Take(3)
+                .ToList();
+            Console.WriteLine(string.Join(" ",numbers));
+        }
+    }
+}
