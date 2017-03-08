e338645b1b6540c8519b3ab44d9b7d4520c07b43
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-03-08 23:42:23 +0200

    Add files via upload

---
+++ Arrays/SievOfEratosthenes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SievOfEratosthenes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            List<int> primeNums = new List<int>();
+            for (int i = 0; i <=num; i++)
+            {
+                for (int j = 2; j <= num; j++)
+                {
+                    if (j >i)
+                    {
+                        break;
+                    }
+                    else if (i == 0 || i == 1)
+                    {
+                        break;
+                    }
+                    else if (i % j == 0 && i != j)
+                    {
+                        break;
+                    }
+                    else if (i == j)
+                    {
+                        primeNums.Add(i);
+                    }
+                }               
+            }
+            foreach (var item in primeNums)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
