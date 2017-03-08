ad1441258b622258365741d0e5a3fc7d5fbed6a3
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-03-08 23:48:39 +0200

    Add files via upload

---
+++ Arrays/FoldAndSum/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoldAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine().Split(' ');
+            int[] arrOfNums = new int[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                arrOfNums[i] = int.Parse(numbers[i]);
+            }
+            int[] firstNumbers = new int[numbers.Length / 4];
+            for (int i = 0; i < numbers.Length / 4; i++)
+            {
+                firstNumbers[i] = arrOfNums[i];
+            }
+            firstNumbers = firstNumbers.Reverse().ToArray();
+            int[] lastNumbers = new int[numbers.Length / 4];
+            int countlastnum = 0;
+
+            for (int i = numbers.Length - 1; i >= numbers.Length - (numbers.Length / 4); i--)
+            {
+                lastNumbers[countlastnum] = arrOfNums[i];
+                countlastnum++;
+            }
+            int[] combined = firstNumbers.Concat(lastNumbers).ToArray();
+
+            int count = 0;
+            int countCombine = 0;
+            int[] sumArray = new int[numbers.Length / 2];
+            for (int i = numbers.Length / 4; i < numbers.Length - (numbers.Length / 4); i++)
+            {
+                
+                    sumArray[count] = arrOfNums[i] + combined[countCombine];
+                    count++;
+                    countCombine++;
+                
+                
+            }
+            foreach (var item in sumArray)
+            {
+                Console.Write(item+" ");
+            }
+            
+        }
+    }
+}
