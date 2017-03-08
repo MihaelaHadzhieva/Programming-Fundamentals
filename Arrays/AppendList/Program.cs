38c46b779febd99d74c84d1ea268e08761201e14
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-03-08 23:51:20 +0200

    Add files via upload

---
+++ Arrays/AppendList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppendList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(new[] { '|', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            var list = new List<int>();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                    list[i] = numbers[i];               
+            }
+            list.Reverse();
+            list.ForEach(Console.WriteLine);
+                
+        }
+    }
+}
