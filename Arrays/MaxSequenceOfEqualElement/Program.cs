a6d437d41b7d44de38cfc6746604ae357d151c60
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-03-08 23:45:18 +0200

    Add files via upload

---
+++ Arrays/MaxSequenceOfEqualElement/Program.cs
@@ -0,0 +1,47 @@
+﻿
+namespace MaxSequnceOfEqualElements
+{
+    using System;
+    using System.Linq;
+ 
+    class MaxSequnceOfEqualElements
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine()
+               .Split(' ')
+               .Select(int.Parse)
+               .ToArray();
+
+            int maxLength = 0;
+            int index = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentLength = 1;
+
+                for (int j = i+1; j < numbers.Length; j++)
+                {                
+                    if (numbers[i] == numbers[j])
+                    {
+                        currentLength++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (currentLength>maxLength)
+                {
+                    maxLength = currentLength;
+                    index = i;
+                }               
+            }
+            for (int i = index; i < index+maxLength; i++)
+            {
+                Console.Write(numbers[i]+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
