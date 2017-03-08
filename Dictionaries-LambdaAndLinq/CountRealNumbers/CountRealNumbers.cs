9b3602f56d30f986897e1c4bd6b91a94af3c061c
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-03-08 23:57:39 +0200

    Add files via upload

---
+++ Dictionaries-LambdaAndLinq/CountRealNumbers/CountRealNumbers.cs
@@ -0,0 +1,36 @@
+﻿
+namespace CountRealNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CountRealNumbers
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToList();
+
+            var countInDic = new SortedDictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (!countInDic.ContainsKey(number))
+                {
+                    countInDic[number] = 0;
+                }
+                countInDic[number]++;
+            }
+
+            foreach (var item in countInDic)
+            {
+                Console.Write(item.Key+" -> ");
+                Console.Write(item.Value);
+                Console.WriteLine();
+            }
+        }
+    }
+}
