9c8b32cc19010302a44500cdb2874212c8b96436
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-03-08 23:44:19 +0200

    Add files via upload

---
+++ Arrays/MostFrequenceElement/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace MaxSequenceOfEqualNumbers
+{
+    using System;
+    using System.Linq;
+
+    class MaxSequenceOfEqualNumbers
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            int maxLength = 0;
+            int index = 0;
+            int startIndex = 0;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int currentLength = 1;
+                for (int j = i + 1; j < numbers.Count; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        currentLength++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (currentLength > maxLength)
+                {
+                    maxLength = currentLength;
+                    index = i;
+                }
+            }
+            for (int i = index; i < index + maxLength; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
